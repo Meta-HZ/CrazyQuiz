0179b98b8b7547e0ff07b2d3256e7bc9d3a11ed8
daanvhekken <Daanvhekken@gmail.com>
2022-01-13 13:41:54 +0100

    Update so live version will rebuild

--- src/gameStart.ts
+++ src/gameStart.ts
@@ -1,6 +1,4 @@
 import Game from './Game.js';
 
-console.log('Javascript is working!');
-
 // Add EventListener to load the game whenever the browser is ready
 window.addEventListener('load',  () => new Game(<HTMLCanvasElement>document.getElementById('canvas')))
