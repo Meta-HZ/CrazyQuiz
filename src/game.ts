ecb2e3938c4240223847af007cc3d3bc61f0bf6a
Daan <90179778+daanvhekken@users.noreply.github.com>
2022-01-13 10:40:11 +0100

    Player can now move

--- src/game.ts
+++ src/game.ts
@@ -1,31 +1,63 @@
 import Player from './Player.js';
 
 export default class Game {
+
   private player: Player;
 
+  private canvas: HTMLCanvasElement;
+  
+  private ctx: CanvasRenderingContext2D;
 
   /**
    * Initialize the Game class
    *
    * @param canvasId id of the canvas
    */
-  public constructor() {
-    this.player = new Player();
-
-    this.displayPlayer()
+  public constructor(canvasId: HTMLCanvasElement) {
+    this.canvas = canvasId;
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
+    
+    this.ctx = <CanvasRenderingContext2D> this.canvas.getContext('2d');
+    
+    this.player = new Player(this.canvas.width, this.canvas.height);
     this.loop();
   }
 
-  private displayPlayer() {
-    let playerNameElement = document.getElementById("playerName")
-    playerNameElement.innerHTML = this.player.name
+  /**
+   * Draws all the necessary elements to the canvas
+   */
+  private draw(): void {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.player.draw(this.ctx);
   }
-  
+
+  /**
+   * Method to move the scoring items
+   */
+  private move(): void {
+    this.player.move();
+  }
+
   /**
    * Method for the Game Loop
    */
   private loop = (): void => {
+    this.move();
+    this.draw();
     requestAnimationFrame(this.loop);
   };
+
+  /**
+   * Method to load an image
+   *
+   * @param source the source
+   * @returns HTMLImageElement - returns an image
+   */
+  public static loadNewImage(source: string): HTMLImageElement {
+    const img = new Image();
+    img.src = source;
+    return img;
+  }
 
 }
--- src/player.ts
+++ src/player.ts
@@ -1,3 +1,6 @@
+import Game from './game.js';
+import KeyboardListener from './KeyboardListener.js';
+
 export default class Player {
     public name: string;
 
@@ -5,11 +8,25 @@ export default class Player {
     
     private score: number;
 
-    private appearanceSource: string;
+    private xPosition: number;
 
-    public constructor() {
+    private yPosition: number;
+
+    private speed: number;
+
+    private keyBoardListener: KeyboardListener;
+
+    private image: HTMLImageElement;
+
+    public constructor(canvasWidth: number, canvasHeight: number) {
         this.name = localStorage.getItem("playerName");
         this.health = 5;
+        this.speed = 4;
+        this.image = Game.loadNewImage('/assets/images/steve-front.png');
+        this.setXPosition(canvasWidth / 2);
+        this.setYPosition(canvasHeight / 2);
+
+        this.keyBoardListener = new KeyboardListener();
     }
 
     public setName(name: string): void {
@@ -20,12 +37,22 @@ export default class Player {
         return this.name;
     }
 
-    public setAppearanceSource(appearanceSource: string): void {
-        this.appearanceSource = appearanceSource;
+    /**
+     * Get the image
+     *
+     * @returns the image of  the GameItem
+     */
+    public getImage(): HTMLImageElement {
+        return this.image;
     }
 
-    private getAppearanceSource(): string {
-        return this.appearanceSource;
+    /**
+     * Set the image of the GameItem
+     *
+     * @param image the image of the GameItem
+     */
+    public setImage(image: HTMLImageElement): void {
+        this.image = image;
     }
 
     private setHealth(health: number): void {
@@ -42,5 +69,88 @@ export default class Player {
 
     private getScore(): number {
         return this.score;
+    }
+
+    /**
+     * Get the xPosition
+     *
+     * @returns returns the position on the x-axis
+     */
+    public getXPosition(): number {
+        return this.xPosition;
+    }
+
+    /**
+     * Set the xPosition
+     *
+     * @param xPosition - set a new xPosition
+     */
+    protected setXPosition(xPosition: number): void {
+        this.xPosition = xPosition;
+    }
+
+    /**
+     * Get the yPosition
+     *
+     * @returns returns the position on the y-axis
+     */
+    public getYPosition(): number {
+        return this.yPosition;
+    }
+
+    /**
+     * Set the yPosition
+     *
+     * @param yPosition - set a new yPosition
+     */
+    protected setYPosition(yPosition: number): void {
+        this.yPosition = yPosition;
+    }
+
+    /**
+     * Get the speed
+     *
+     * @returns returns the speed
+     */
+    public getSpeed(): number {
+        return this.speed;
+    }
+
+    /**
+     * Set the speed
+     *
+     * @param speed - set a new speed
+     */
+    protected setSpeed(speed: number): void {
+        this.speed = speed;
+    }
+
+    /**
+    * Method to move the player on the canvas
+    */
+    public move(): void {
+        if (this.keyBoardListener.isKeyDown(KeyboardListener.KEY_LEFT)) {
+            this.xPosition = this.xPosition - this.speed;
+        }
+        if (this.keyBoardListener.isKeyDown(KeyboardListener.KEY_RIGHT)) {
+            this.xPosition = this.xPosition + this.speed
+        }
+        if (this.keyBoardListener.isKeyDown(KeyboardListener.KEY_UP)) {
+            this.yPosition = this.yPosition - this.speed;
+            this.image = Game.loadNewImage('/assets/images/steve-back.png');
+        }
+        if (this.keyBoardListener.isKeyDown(KeyboardListener.KEY_DOWN)) {
+            this.yPosition = this.yPosition + this.speed;
+            this.image = Game.loadNewImage('/assets/images/steve-front.png');
+        }
+    }
+
+    /**
+     * Draw the GameItem on the canvas
+     *
+     * @param ctx rendering context
+     */
+    public draw(ctx: CanvasRenderingContext2D): void {
+        ctx.drawImage(this.image, this.xPosition, this.yPosition);
     }
 }
